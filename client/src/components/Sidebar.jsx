4286965130cb698ac9c02868ebe454ed4fec8b17
asad851 <siddiqui851@gmail.com>
2025-05-01 11:22:59 +0530

    add file/folder upload to sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -9,22 +9,45 @@ import {
 import { Button } from "./ui/button";
 import { File, PlusCircleIcon } from "lucide-react";
 import { useGetSegmentQuery } from "@/helper/apis/folder/setup";
+import { useCreateSegmentApi } from "@/helper/apis/folder/folder";
+import { useDropzone } from "react-dropzone";
+import { useRef } from "react";
 
 export default function AppSidebar() {
-   const { data, error, isLoading } = useGetSegmentQuery();
+  const folderInputRef = useRef(null);
+  const fileInputRef = useRef(null);
+  const { data, error, isLoading } = useGetSegmentQuery();
+  const { acceptedFiles, fileRejections, getRootProps, getInputProps } =
+    useDropzone({ onDrop });
+  const { handleCreateSegment } = useCreateSegmentApi();
+
+  function onDrop(acceptedFiles, event) {
+    handleCreateSegment(acceptedFiles);
+    // console.log(acceptedFiles)
+  }
+
   return (
     <div className="min-h-full w-80 py-5 ">
       <div className="py-5 border-b-2 flex flex-col gap-5">
-        <Button>
+        <div className="hidden " {...getRootProps()}>
+          <input {...getInputProps({})} multiple ref={fileInputRef} />
+          <input
+            type="file"
+            webkitdirectory=""
+            hidden
+            {...getInputProps({})}
+            ref={folderInputRef}
+          />
+        </div>
+        <Button onClick={() => folderInputRef.current.click()}>
           Add folder
           <PlusCircleIcon />
         </Button>
-      
-          <Button>
-            Add file
-            <File />
-          </Button>
-     
+
+        <Button onClick={() => fileInputRef.current.click()}>
+          Add file
+          <File />
+        </Button>
       </div>
     </div>
   );
