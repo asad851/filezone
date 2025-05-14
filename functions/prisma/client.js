8ff5bcefbd8d5fe5a116fda5003b33e24f814552
asad851 <siddiqui851@gmail.com>
2025-05-14 13:19:55 +0530

    remove

--- functions/prisma/client.js
+++
@@ -1,8 +0,0 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
-
-export const prisma = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prisma;
-}
--- functions/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-const secretkey = process.env.JWT_SECRET;
-
-export default function generateToken(data) {
-  return jwt.sign(data, secretkey);
-}
