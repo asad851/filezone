baf6fad8ab087bc15a514bd94962f58c2d51221a
asad851 <siddiqui851@gmail.com>
2025-05-16 16:04:13 +0530

    add logout function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Avatar } from "./ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -17,8 +17,18 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Cookies from "js-cookie";
+import { logoutUser } from "@/store/auth/authSlice";
 function Header() {
+  const dispatch = useDispatch();
+
   const { userData } = useSelector((state) => state.auth);
+  const handleLogout = () => {
+    console.log("sfds");
+    Cookies.remove("token");
+    localStorage.clear();
+    dispatch(logoutUser());
+  };
   return (
     <header className="  w-full sticky top-0">
       <nav className=" py-2 px-4 flex justify-between items-center ">
@@ -52,7 +62,9 @@ function Header() {
             </DropdownMenuGroup>
 
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Log out</DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => handleLogout()}>
+              Log out
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </nav>
